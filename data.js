e5db4f5f58cca85ddd665235193ec498360be329
bhargavlakkaraju <85938339+bhargavlakkaraju@users.noreply.github.com>
2021-06-15 16:57:47 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,430 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-shutterstock_1917933104",
+      "name": "shutterstock_1917933104",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0619262119013477,
+        "pitch": 0.027751835244465894,
+        "fov": 1.566717244256129
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.008023892767274887,
+          "pitch": -0.2992596267728871,
+          "rotation": 0,
+          "target": "1-shutterstock_736756324"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1369920226259778,
+          "pitch": -0.2311365963964036,
+          "title": "Power your future",
+          "text": "Welcome to the Future! Experience it now!"
+        },
+        {
+          "yaw": -1.6946347327523839,
+          "pitch": -0.25399533982725586,
+          "title": "Technological breakthrough",
+          "text": "This is the time for a technological breakthrough that amends our course of actions."
+        },
+        {
+          "yaw": -0.011417544905125254,
+          "pitch": -0.6549738457636511,
+          "title": "Step into the Future!",
+          "text": "The future lies ahead"
+        }
+      ]
+    },
+    {
+      "id": "1-shutterstock_736756324",
+      "name": "shutterstock_736756324",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.5410902210098474,
+        "pitch": -0.12779667085720092,
+        "fov": 1.566717244256129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0012332891407922375,
+          "pitch": 0.16066446024210457,
+          "rotation": 0,
+          "target": "2-shutterstock_1488050153"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3171220087017552,
+          "pitch": -0.38214531828733733,
+          "title": "The future is here",
+          "text": "<font face=\"Helvetica\"><span style=\"font-size: 10.6667px;\"><i>The barriers will now stumble and they will fall.</i></span></font><br>"
+        },
+        {
+          "yaw": 3.124715037913253,
+          "pitch": -0.004645134693493702,
+          "title": "Far yet near",
+          "text": "The Future is far, yet near!"
+        },
+        {
+          "yaw": -0.007777777707397959,
+          "pitch": -0.140537310919008,
+          "title": "A guiding light",
+          "text": "The future is like a guiding light from a tunnel! Step into it now!"
+        }
+      ]
+    },
+    {
+      "id": "2-shutterstock_1488050153",
+      "name": "shutterstock_1488050153",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.019392547244379443,
+        "pitch": -0.12109891743043022,
+        "fov": 1.566717244256129
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1505476511712232,
+          "pitch": 0.24515755842282516,
+          "rotation": 0,
+          "target": "3-shutterstock_1104865223"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.008298208108984184,
+          "pitch": -0.26496992727885704,
+          "title": "Light of your life",
+          "text": "Your Future is the light of your life!"
+        },
+        {
+          "yaw": 1.716247942348864,
+          "pitch": -0.07346978937442827,
+          "title": "The search is always on!",
+          "text": "We always keep searching for our future in the present!"
+        },
+        {
+          "yaw": 3.0479351909360215,
+          "pitch": -0.05476717252552632,
+          "title": "Future lies ahead",
+          "text": "But the future always lies ahead of time!"
+        },
+        {
+          "yaw": -1.1783204728181573,
+          "pitch": -0.10691181381166714,
+          "title": "Future is the power",
+          "text": "Your future is the power in your hands"
+        }
+      ]
+    },
+    {
+      "id": "3-shutterstock_1104865223",
+      "name": "shutterstock_1104865223",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5215462575700904,
+          "pitch": 0.15077219526303942,
+          "rotation": 0,
+          "target": "4-shutterstock_200000909"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0334837995620116,
+          "pitch": -0.02076439570088162,
+          "title": "The path for Future",
+          "text": "Presenting the path for your Future"
+        },
+        {
+          "yaw": -2.0143294080912675,
+          "pitch": -0.013699389083894786,
+          "title": "power to dream",
+          "text": "Your dreams have the power to drive your Future"
+        },
+        {
+          "yaw": -0.0740465545459923,
+          "pitch": -0.09831215689418649,
+          "title": "nothing here",
+          "text": "Search for your future!"
+        },
+        {
+          "yaw": 1.5379246685672303,
+          "pitch": -0.1372690659540705,
+          "title": "The future of protection",
+          "text": "This is the direction to protect your future"
+        }
+      ]
+    },
+    {
+      "id": "4-shutterstock_200000909",
+      "name": "shutterstock_200000909",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08985453949189193,
+          "pitch": 0.30323098908382207,
+          "rotation": 0,
+          "target": "5-shutterstock_528340795"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09249878015494417,
+          "pitch": -0.06649290847363609,
+          "title": "Welcome to the Future",
+          "text": "Welcome to the Future of Protection"
+        }
+      ]
+    },
+    {
+      "id": "5-shutterstock_528340795",
+      "name": "shutterstock_528340795",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1269735469145807,
+          "pitch": 0.13161247824206868,
+          "rotation": 0,
+          "target": "6-shutterstock_1937790310"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5920065119434792,
+          "pitch": -0.2828267145819332,
+          "title": "The Future is here",
+          "text": "Power your Future with Voliam Targo™"
+        },
+        {
+          "yaw": 0.051904810012491254,
+          "pitch": -0.25972260637540145,
+          "title": "Voliam Targo",
+          "text": "An insecticide that is disruptive"
+        },
+        {
+          "yaw": -3.136220746390009,
+          "pitch": -0.16547738280118907,
+          "title": "Choose it right!",
+          "text": "Voliam Targo™ is an insecticide that empowers the growers and makes them realise that the journey they embark into the future is free from pests, free from troubles and in all a rewarding journey."
+        }
+      ]
+    },
+    {
+      "id": "6-shutterstock_1937790310",
+      "name": "shutterstock_1937790310",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0053775705041295,
+          "pitch": -0.22424426348412752,
+          "rotation": 0,
+          "target": "7-shutterstock_1922697218"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.320493302481017,
+          "pitch": -0.19200040687890763,
+          "title": "Flawless harvest",
+          "text": "Be rest assured to get a flawless harvest and maximum yields in your chilli crop"
+        }
+      ]
+    },
+    {
+      "id": "7-shutterstock_1922697218",
+      "name": "shutterstock_1922697218",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.359645613173992,
+        "pitch": -0.312838870028596,
+        "fov": 1.566717244256129
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7095471938850579,
+          "pitch": -0.41948528889514236,
+          "title": "The Future is Today, The Future is Voliam Targo !",
+          "text": "Voliam Targo™ powers your future by setting new standards in market as a single shot solution for Leps, Thrips &amp; Mites"
+        },
+        {
+          "yaw": -1.3191260378497685,
+          "pitch": 0.0986755552608507,
+          "title": "Voliam Targo gives power to your crop!",
+          "text": "<font face=\"Helvetica\"><span style=\"font-size: 10.6667px;\">So, to all the Chilli growers of India, this is the time to Power your today, the time to Power your dreams and this is the time to Power your Future!</span></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Voliam Targo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
